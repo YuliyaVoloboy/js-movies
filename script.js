2bf11628a997dce6a6610794c258cd0f1a1ae1c6
Yulia <yvoloboy@gmail.com>
2021-02-09 22:11:07 +0300

    while

--- script.js
+++ script.js
@@ -25,6 +25,22 @@ for (let i = 0; i < 2; i++) {
     personalMovieDB.movies[a] = b;
 }
 
+let i = 2;
+
+while (i < 2) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a !='' && b !='' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }  
+        personalMovieDB.movies[a] = b;
+};
+
 if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
